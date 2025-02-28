4c3ff0ec00234faf9fce09eab1be104816de08c2
Martin Bruzina <martin@bruzina.cz>
2025-02-28 17:40:29 +0100

    feat: add s3 backend

--- config.tf
+++ config.tf
@@ -6,6 +6,19 @@ terraform {
       version = ">= 6.0.0"
     }
   }
+
+  backend "s3" {
+    bucket       = "xebis-terraform"
+    key          = "github-xebis"
+    use_lockfile = true # Set to false only for non-AWS S3 compatible APIs without "conditional object PUTs" capability
+
+    # Only for non-AWS S3 compatible APIs
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+  }
 }
 
 provider "github" {
