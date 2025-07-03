8432c98037b7e2a273805b143dc7e1e7a96aa7dc
Martin Bruzina <martin@bruzina.cz>
2025-07-03 18:21:55 +0200

    feat: add organization item, rename default props to repo defaults

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 locals {
   config             = yamldecode(file(var.path))
-  default_properties = try(local.config.default-properties, null)
+  repository_defaults = try(local.config.organization.repository-defaults, null)
   repositories       = local.config.repositories
 }
 
@@ -14,19 +14,19 @@ resource "github_repository" "repo" {
   topics       = try(each.value.topics, null)
 
   # Properties
-  visibility  = try(each.value.visibility, local.default_properties.visibility, null)
+  visibility  = try(each.value.visibility, local.repository_defaults.visibility, null)
   is_template = try(each.value.is_template, null)
 
   # Features
-  has_issues      = try(each.value.has_issues, local.default_properties.has_issues, null)
-  has_discussions = try(each.value.has_discussions, local.default_properties.has_discussions, null)
-  has_projects    = try(each.value.has_projects, local.default_properties.has_projects, null)
-  has_wiki        = try(each.value.has_wiki, local.default_properties.has_wiki, null)
+  has_issues      = try(each.value.has_issues, local.repository_defaults.has_issues, null)
+  has_discussions = try(each.value.has_discussions, local.repository_defaults.has_discussions, null)
+  has_projects    = try(each.value.has_projects, local.repository_defaults.has_projects, null)
+  has_wiki        = try(each.value.has_wiki, local.repository_defaults.has_wiki, null)
 
   # Settings
-  allow_merge_commit     = try(each.value.allow_merge_commit, local.default_properties.allow_merge_commit, null)
-  allow_squash_merge     = try(each.value.allow_squash_merge, local.default_properties.allow_squash_merge, null)
-  allow_rebase_merge     = try(each.value.allow_rebase_merge, local.default_properties.allow_rebase_merge, null)
-  allow_auto_merge       = try(each.value.allow_auto_merge, local.default_properties.allow_auto_merge, null)
-  delete_branch_on_merge = try(each.value.delete_branch_on_merge, local.default_properties.delete_branch_on_merge, null)
+  allow_merge_commit     = try(each.value.allow_merge_commit, local.repository_defaults.allow_merge_commit, null)
+  allow_squash_merge     = try(each.value.allow_squash_merge, local.repository_defaults.allow_squash_merge, null)
+  allow_rebase_merge     = try(each.value.allow_rebase_merge, local.repository_defaults.allow_rebase_merge, null)
+  allow_auto_merge       = try(each.value.allow_auto_merge, local.repository_defaults.allow_auto_merge, null)
+  delete_branch_on_merge = try(each.value.delete_branch_on_merge, local.repository_defaults.delete_branch_on_merge, null)
 }
