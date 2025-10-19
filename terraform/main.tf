c89db1399fa7bfc590ba395381d53a93a0d5bcd8
Martin Bruzina <martin@bruzina.cz>
2025-10-19 17:32:25 +0200

    refactor: repos config as local instead of processing inputs in resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,34 @@
 locals {
-  config           = yamldecode(file(var.path))
+  config = yamldecode(file(var.path))
+
+  repositories = [
+    for repository in try(local.config.repositories, []) : {
+      # Metadata
+      name         = repository.name
+      description  = try(repository.description, null)
+      homepage_url = try(repository.homepage_url, null)
+      topics       = try(repository.topics, null)
+      # Properties
+      visibility  = try(repository.visibility, local.config.organization.all-repositories.visibility, null)
+      is_template = try(repository.is_template, null)
+      # Features
+      has_issues      = try(repository.has_issues, local.config.organization.all-repositories.has_issues, null)
+      has_discussions = try(repository.has_discussions, local.config.organization.all-repositories.has_discussions, null)
+      has_projects    = try(repository.has_projects, local.config.organization.all-repositories.has_projects, null)
+      has_wiki        = try(repository.has_wiki, local.config.organization.all-repositories.has_wiki, null)
+      # Settings
+      allow_merge_commit     = try(repository.allow_merge_commit, local.config.organization.all-repositories.allow_merge_commit, null)
+      allow_squash_merge     = try(repository.allow_squash_merge, local.config.organization.all-repositories.allow_squash_merge, null)
+      allow_rebase_merge     = try(repository.allow_rebase_merge, local.config.organization.all-repositories.allow_rebase_merge, null)
+      allow_auto_merge       = try(repository.allow_auto_merge, local.config.organization.all-repositories.allow_auto_merge, null)
+      delete_branch_on_merge = try(repository.delete_branch_on_merge, local.config.organization.all-repositories.delete_branch_on_merge, null)
+    }
+  ]
+
   all_repositories = try(local.config.organization.all-repositories, [])
-  repositories     = try(local.config.repositories, [])
+  repositories0    = try(local.config.repositories, [])
   all_repositories_rulesets = [
-    for pair in try(setproduct(local.repositories, local.all_repositories.rulesets), []) : {
+    for pair in try(setproduct(local.repositories0, local.all_repositories.rulesets), []) : {
       repository = pair[0],
       ruleset    = pair[1]
     }
@@ -11,30 +36,30 @@ locals {
 }
 
 resource "github_repository" "repo" {
-  for_each = { for repo in local.repositories : repo.name => repo }
+  for_each = { for repository in local.repositories : repository.name => repository }
 
   # Metadata
   name         = each.value.name
-  description  = try(each.value.description, null)
-  homepage_url = try(each.value.homepage_url, null)
-  topics       = try(each.value.topics, null)
+  description  = each.value.description
+  homepage_url = each.value.homepage_url
+  topics       = each.value.topics
 
   # Properties
-  visibility  = try(each.value.visibility, local.all_repositories.visibility, null)
-  is_template = try(each.value.is_template, null)
+  visibility  = each.value.visibility
+  is_template = each.value.is_template
 
   # Features
-  has_issues      = try(each.value.has_issues, local.all_repositories.has_issues, null)
-  has_discussions = try(each.value.has_discussions, local.all_repositories.has_discussions, null)
-  has_projects    = try(each.value.has_projects, local.all_repositories.has_projects, null)
-  has_wiki        = try(each.value.has_wiki, local.all_repositories.has_wiki, null)
+  has_issues      = each.value.has_issues
+  has_discussions = each.value.has_discussions
+  has_projects    = each.value.has_projects
+  has_wiki        = each.value.has_wiki
 
   # Settings
-  allow_merge_commit     = try(each.value.allow_merge_commit, local.all_repositories.allow_merge_commit, null)
-  allow_squash_merge     = try(each.value.allow_squash_merge, local.all_repositories.allow_squash_merge, null)
-  allow_rebase_merge     = try(each.value.allow_rebase_merge, local.all_repositories.allow_rebase_merge, null)
-  allow_auto_merge       = try(each.value.allow_auto_merge, local.all_repositories.allow_auto_merge, null)
-  delete_branch_on_merge = try(each.value.delete_branch_on_merge, local.all_repositories.delete_branch_on_merge, null)
+  allow_merge_commit     = each.value.allow_merge_commit
+  allow_squash_merge     = each.value.allow_squash_merge
+  allow_rebase_merge     = each.value.allow_rebase_merge
+  allow_auto_merge       = each.value.allow_auto_merge
+  delete_branch_on_merge = each.value.delete_branch_on_merge
 }
 
 resource "github_repository_ruleset" "all_repositories" {
